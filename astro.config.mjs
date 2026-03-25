c752d30bb613bde383a539dfe3a8fff52730d416
Anis Haned <anis.haned@davidson.fr>
2026-03-26 00:41:27 +0100

    Initial commit: Eruption Fibre website (Astro + GitHub Pages)

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://yanhnd.github.io',
+  base: '/Eruption-fibre',
+});
